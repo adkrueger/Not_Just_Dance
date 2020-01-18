309af5c46ba3e29a46d9907df80b201650c9ee2c
adkrueger <adkrueger@wpi.edu>
2020-01-18 17:28:55 -0500

    merged branches, project up to date with webcam

--- get_closest_pose.js
+++ get_closest_pose.js
@@ -1,53 +1,32 @@
-//get closest taken pose in array, return everything after the number
-//json.parse
+function binarySearch(arr, target, start, end) {
+    let midpoint = Math.floor((start + end) / 2);
 
-/*
+    if (arr[midpoint] === target) {
+        return midpoint;
+    }
+    if (start > end) {
+        return midpoint;
+    }
 
-*/
-
-
-/*
-
-var counts = json,
-  goal = n;
-
-var closest = counts.reduce(function(prev, curr) {
-  return (Math.abs(curr - goal) < Math.abs(prev - goal) ? curr : prev);
-});
-
-console.log(closest);
-*/
-
-
-function binarySearch(arr, target, start, end){
-  let midpoint = Math.floor((start + end) / 2);
-
-  if (arr[midpoint] == target){
-    return midpoint;
-  }
-  if (start > end){
-    return midpoint;
-  }
-
-  if (arr[midpoint] > target){
-    return binarySearch(arr, target, start, midpoint - 1);
-  } else if (arr[midpoint] < target){
-    return binarySearch(arr, target, midpoint + 1, end);
-  }
+    if (arr[midpoint] > target) {
+        return binarySearch(arr, target, start, midpoint - 1);
+    } else if (arr[midpoint] < target) {
+        return binarySearch(arr, target, midpoint + 1, end);
+    }
 }
 
-  function get_closest_pose(n) {
+function get_closest_pose(n) {
     fetch("wholeDance.json")
-    .then(response => response.json())
-    .then((json) => {
-      console.log(json);
-      let times = new Array(json.length);
-      json.forEach((arr, i) => {
-        times[i] = arr[0];
-      });
-      let closestIndex = binarySearch(times, n, 0, times.length);
-      console.log(json[closestIndex]);
-    });
+        .then(response => response.json())
+        .then((json) => {
+            console.log(json);
+            let times = new Array(json.length);
+            json.forEach((arr, i) => {
+                times[i] = arr[0];
+            });
+            let closestIndex = binarySearch(times, n, 0, times.length);
+            console.log(json[closestIndex]);
+        });
 
     /*
     
@@ -56,6 +35,6 @@ function binarySearch(arr, target, start, end){
     const obj = JSON.parse(pose);
     console.log(obj);
     */
-  }
+}
 
-  get_closest_pose(6969);
+get_closest_pose(6969);
--- posenet2.js
+++ posenet2.js
@@ -1,109 +1,110 @@
-const videoWidth = 600;
-const videoHeight = 500;
+window.onload = function() {
+  const videoWidth = 600;
+  const videoHeight = 500;
 
-async function setupCamera() {
-  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
-    throw new Error(
-        'Browser API navigator.mediaDevices.getUserMedia not available');
+  async function setupCamera() {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+      throw new Error(
+          'Browser API navigator.mediaDevices.getUserMedia not available');
+    }
+
+    const video = document.getElementById('video');
+    video.width = videoWidth;
+    video.height = videoHeight;
+
+    const mobile = false;
+    const stream = await navigator.mediaDevices.getUserMedia({
+      'audio': false,
+      'video': {
+        facingMode: 'user',
+        width: mobile ? undefined : videoWidth,
+        height: mobile ? undefined : videoHeight,
+      },
+    });
+    video.srcObject = stream;
+
+    return new Promise((resolve) => {
+      video.onloadedmetadata = () => {
+        resolve(video);
+      };
+    });
   }
 
-  const video = document.getElementById('video');
-  video.width = videoWidth;
-  video.height = videoHeight;
 
-  const mobile = false;
-  const stream = await navigator.mediaDevices.getUserMedia({
-    'audio': false,
-    'video': {
-      facingMode: 'user',
-      width: mobile ? undefined : videoWidth,
-      height: mobile ? undefined : videoHeight,
-    },
-  });
-  video.srcObject = stream;
+  async function loadVideo() {
+    const video = await setupCamera();
+    video.play();
 
-  return new Promise((resolve) => {
-    video.onloadedmetadata = () => {
-      resolve(video);
-    };
-  });
-}
+    return video;
+  }
 
+  function detectPoseInRealTime(video, net) {
+    const canvas = document.getElementById('output');
+    const ctx = canvas.getContext('2d');
 
-async function loadVideo() {
-  const video = await setupCamera();
-  video.play();
+    const flipPoseHorizontal = true;
 
-  return video;
-}
+    canvas.width = videoWidth;
+    canvas.height = videoHeight;
 
-function detectPoseInRealTime(video, net) {
-  const canvas = document.getElementById('output');
-  const ctx = canvas.getContext('2d');
+    async function poseDetectionFrame() {
+      let all_poses = await net.estimatePoses(video, {
+        flipHorizontal: flipPoseHorizontal,
+        decodingMethod: 'multi-person'
+      });
 
-  const flipPoseHorizontal = true;
+      console.log(all_poses);
 
-  canvas.width = videoWidth;
-  canvas.height = videoHeight;
+      ctx.clearRect(0, 0, videoWidth, videoHeight);
+      ctx.fillStyle = "black";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  async function poseDetectionFrame() {
-    let all_poses = await net.estimatePoses(video, {
-            flipHorizontal: flipPoseHorizontal,
-            decodingMethod: 'multi-person'
-    });
+      ctx.save();
+      ctx.scale(-1, 1);
+      ctx.translate(-videoWidth, 0);
+      //ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
+      ctx.restore();
+      /*
 
-    console.log(all_poses);
-
-    ctx.clearRect(0, 0, videoWidth, videoHeight);
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    ctx.save();
-    ctx.scale(-1, 1);
-    ctx.translate(-videoWidth, 0);
-    //ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
-    ctx.restore();
-    /*
-
-    if(all_poses.length > 0) {
-      drawSkeleton(all_poses[0].keypoints, ctx);
-      drawKeypoints(all_poses[0].keypoints, ctx);
-    }
-    */
+      if(all_poses.length > 0) {
+        drawSkeleton(all_poses[0].keypoints, ctx);
+        drawKeypoints(all_poses[0].keypoints, ctx);
+      }
+      */
 
 
-
-    all_poses.forEach(({score, keypoints}) => {
+      all_poses.forEach(({score, keypoints}) => {
         drawSkeleton(keypoints, ctx);
         drawKeypoints(keypoints, ctx);
+      });
+
+
+      requestAnimationFrame(poseDetectionFrame);
+    }
+
+    poseDetectionFrame();
+  }
+
+  async function bindPage() {
+    const net = await posenet.load({
+      architecture: 'ResNet50',
+      outputStride: 32,
+      inputResolution: {width: 257, height: 200},
+      quantBytes: 2
     });
 
+    let video;
 
-    requestAnimationFrame(poseDetectionFrame);
+    try {
+      video = await loadVideo();
+    } catch (e) {
+      console.log("Video capture failed");
+      throw e;
+    }
+
+    detectPoseInRealTime(video, net);
   }
 
-  poseDetectionFrame();
+  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+  bindPage();
 }
-
-async function bindPage() {
-  const net = await posenet.load({
-    architecture: 'ResNet50',
-    outputStride: 32,
-    inputResolution: { width: 257, height: 200 },
-    quantBytes: 2
-  });
-
-  let video;
-
-  try {
-    video = await loadVideo();
-  } catch (e) {
-    console.log("Video capture failed");
-    throw e;
-  }
-
-  detectPoseInRealTime(video, net);
-}
-
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-bindPage();
