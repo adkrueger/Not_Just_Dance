574d42dc3bf97b4974df3d410a5cc08926701192
adkrueger <adkrueger@wpi.edu>
2020-01-18 17:09:27 -0500

    stylistic changes, added new close ability for library

--- sidenav.js
+++ sidenav.js
@@ -1,7 +1,16 @@
+let opened = false;
+
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "45%";
-    document.getElementById("main").style.marginLeft = "45%";
+    if(opened) {
+        closeNav();
+        opened = false;
+    }
+    else {
+        document.getElementById("mySidenav").style.width = "45%";
+        document.getElementById("main").style.marginLeft = "45%";
+        opened = true
+    }
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
